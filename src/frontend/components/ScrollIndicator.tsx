8148b96e167f0ccf443deada7c9b205edde99b1b
cjnix35 <doge.perseverance@gmail.com>
2023-04-09 21:58:19 +0300

    change size of scroll indicator

--- src/frontend/components/ScrollIndicator.tsx
+++ src/frontend/components/ScrollIndicator.tsx
@@ -10,16 +10,20 @@ const ScrollIndicator: React.FC<ScrollIndicatorProps> = ({ isVisible }) => {
     <>
       {
         <svg
-          width={60}
-          height={60}
+          width={48}
+          height={48}
           viewBox="0 0 20 20"
-          fill="red"
           className={`${
             isVisible ? "rotate-0" : "rotate-180"
           } transition-all duration-200 ease-in-out`}
           xmlns="http://www.w3.org/2000/svg"
         >
-          <path d="M1 6L10 16L19 6H1Z" />
+          <path
+            d="M1 6L10 16L19 6H1Z"
+            fill="rgba(255,255,255,0.50)"
+            stroke="rgba(196,126,252,0.7)"
+            strokeWidth="1"
+          />
         </svg>
       }
     </>
